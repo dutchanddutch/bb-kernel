c43c563f4164ad56b5d5854133673d6eed9dfe17
Matthijs van Duin <matthijsvanduin@gmail.com>
2016-06-04 14:13:20 +0200

    enhance patch.sh to make patch management easier
    Instead of having to manually listing each individual patch, you can
    just point it to directories containing patches to be applied (this is
    not recursive).  Directories without patches are no problem.
    
    This should also reduce the amount of conflicts when rebasing.
    (except when rebasing *this* patch)

--- patch.sh
+++ patch.sh
@@ -22,6 +22,8 @@
 
 # Split out, so build_kernel.sh and build_deb.sh can share..
 
+shopt -s nullglob
+
 . ${DIR}/version.sh
 if [ -f ${DIR}/system.sh ] ; then
 	. ${DIR}/system.sh
@@ -64,6 +66,28 @@ cleanup () {
 		fi
 	fi
 	exit 2
+}
+
+dir () {
+	wdir="$1"
+	if [ -d "${DIR}/patches/$wdir" ]; then
+		echo "dir: $wdir"
+
+		if [ "x${regenerate}" = "xenable" ] ; then
+			start_cleanup
+		fi
+
+		number=
+		for p in "${DIR}/patches/$wdir/"*.patch; do
+			${git} "$p"
+			number=$(( $number + 1 ))
+		done
+
+		if [ "x${regenerate}" = "xenable" ] ; then
+			cleanup
+		fi
+	fi
+	unset wdir
 }
 
 cherrypick () {
@@ -186,15 +210,9 @@ rt () {
 	${git} "${DIR}/patches/rt/0001-merge-CONFIG_PREEMPT_RT-Patch-Set.patch"
 }
 
-local_patch () {
-	echo "dir: dir"
-	${git} "${DIR}/patches/dir/0001-patch.patch"
-}
-
 #external_git
 #aufs4
 #rt
-#local_patch
 
 reverts () {
 	echo "dir: reverts"
@@ -259,19 +277,6 @@ backports () {
 		post_backports
 	fi
 	patch_backports
-}
-
-fixes () {
-	echo "dir: fixes"
-	#regenerate="enable"
-	if [ "x${regenerate}" = "xenable" ] ; then
-		start_cleanup
-	fi
-
-	if [ "x${regenerate}" = "xenable" ] ; then
-		number=1
-		cleanup
-	fi
 }
 
 ti () {
@@ -780,7 +785,7 @@ more_fixes () {
 ###
 reverts
 #backports
-#fixes
+dir 'fixes'
 ti
 pru_uio
 pru_rpmsg
@@ -788,6 +793,7 @@ bbb_overlays
 beaglebone
 quieter
 more_fixes
+dir 'local'
 
 packaging () {
 	echo "dir: packaging"
